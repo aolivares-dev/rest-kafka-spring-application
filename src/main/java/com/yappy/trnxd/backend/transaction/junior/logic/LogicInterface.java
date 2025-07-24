135661c0b61faf4dc8d58e4381ff1cdb4dcb72d6
Alessandro Olivares <aolivares@bgeneral.com>
2025-07-24 13:38:58 -0500

    feat: haciendo cambio para ver como funciona la rama

--- src/main/java/com/yappy/trnxd/backend/transaction/junior/logic/LogicInterface.java
+++ src/main/java/com/yappy/trnxd/backend/transaction/junior/logic/LogicInterface.java
@@ -2,4 +2,6 @@ package com.yappy.trnxd.backend.transaction.junior.logic;
 
 public interface LogicInterface {
     String example(String string);
+    // añade otro metodo
+    String anotherExample(String string, int number);
 }
